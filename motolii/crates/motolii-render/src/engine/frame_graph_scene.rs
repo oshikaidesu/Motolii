d847f51b8e1a4abbd2dc3c21fe29bd63ddcae26a
oshikaidesu <oshikai.desu@gmail.com>
2026-09-22 15:09:33 +0900

    gpu: remove legacy scene assembly module implementation

--- motolii/crates/motolii-render/src/engine/frame_graph_scene.rs
+++ motolii/crates/motolii-render/src/engine/frame_graph_scene.rs
@@ -1,13 +1,2 @@
-use crate::doc::core::{CompSpec, LayerPlacement, ResolvedCamera};
-use crate::doc::store::LayerId;
-use crate::frame_graph::{SceneContentValue, SceneImageSourceValue, SceneLayerValue, SceneValue, SolverPlanValue};
-use crate::render::compositor::{BlendMode as CompositeBlendMode, Layer, LayerWithPasses};
-use crate::render::engine::{Engine, EngineError};
-
-impl Engine {
-
-}
-
-
 #[cfg(test)]
 mod tests;
